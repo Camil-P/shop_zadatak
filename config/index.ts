a45639b24e5555438dc06c0c7e8d3ec3c9417cda
Camil-P <56632088+Camil-P@users.noreply.github.com>
2023-12-17 20:54:08 +0100

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,7 @@
 export default {
   port: 8000,
   host: "localhost",
-  dbUrl:
-    "mongodb+srv://YoungOldhead:Dzabaluka@cluster0.rzz07qa.mongodb.net/?retryWrites=true&w=majority",
+  dbUrl: "",
   privateKey: "shopZadatak",
   accessTokenTtl: "10m",
   refreshTokenTtl: "1y",
